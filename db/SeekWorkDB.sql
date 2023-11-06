0cfa03084e94007026101507a267b76f20664be6
chathura226 <chathuralakshan226@gmail.com>
2023-11-06 19:54:18 +0530

    some changes to docker

--- db/SeekWorkDB.sql
+++ db/SeekWorkDB.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.1.0, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.2.0, for Linux (x86_64)
 --
 -- Host: localhost    Database: SeekWorkDB
 -- ------------------------------------------------------
--- Server version	8.1.0
+-- Server version	8.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -624,4 +624,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-05 15:42:13
+-- Dump completed on 2023-11-06 14:23:10
