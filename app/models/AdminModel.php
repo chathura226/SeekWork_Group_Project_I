02dbf7ab2a551e8ab46df278c562080bfedd277a
chathura226 <chathuralakshan226@gmail.com>
2023-11-12 20:17:04 +0530

    update profile validations of admin

--- app/models/AdminModel.php
+++ app/models/AdminModel.php
@@ -18,4 +18,28 @@ class AdminModel extends Model {
     ];
 
 
+     //if all goes well, return true, else false
+     public function validate($data){
+        $this->errors=[];
+
+         
+
+
+        if(empty($data['firstName'])){
+            $this->errors['firstName']="First Name is required!";
+        }
+        if(empty($data['lastName'])){
+            $this->errors['lastName']="Last Name is required!";
+        }
+        if(empty($data['address'])){
+            $this->errors['address']="Address is required!";
+        }
+        
+
+        if(empty($this->errors)){
+            return true;
+        }
+        
+        return false;
+    }
 }
