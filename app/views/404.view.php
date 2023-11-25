6c33957b806d9fff59149da31671ae42ac008b82
chathura226 <chathuralakshan226@gmail.com>
2023-11-25 11:54:26 +0530

    404

--- app/views/404.view.php
+++ app/views/404.view.php
@@ -1,12 +1,14 @@
-<?php $this->view('includes/header',$data) ?>
-<?php $this->view('includes/nav',$data) ?>
+<?php $this->view('includes/header', $data) ?>
+<?php $this->view('includes/nav', $data) ?>
 
 <div class="flex flex-column c-s-1 c-e-13 align-items-center">
-<h1>Page not found.</h1>
-<img src="<?=ROOT?>/assets/images/404_cat.png" style="max-height:573px; max-width:510px;" alt="404">
-<a href="<?=ROOT?>"><h1>Go to Home</h1></a>
+    <br>
+    <img src="<?= ROOT ?>/assets/images/404_cat.png" style="max-height:573px; max-width:510px;" alt="404">
+    <a href="<?= ROOT ?>">
+        <h1>Go to Home</h1>
+    </a>
 
 </div>
 
 
-<?php $this->view("includes/footer",$data);
+<?php $this->view("includes/footer", $data);
