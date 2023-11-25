e24a722d373cb109473b022fcd21a769478057bf
chathura226 <chathuralakshan226@gmail.com>
2023-11-25 11:53:44 +0530

    404 image and necessy text

--- app/views/404.view.php
+++ app/views/404.view.php
@@ -1,9 +1,11 @@
 <?php $this->view('includes/header',$data) ?>
 <?php $this->view('includes/nav',$data) ?>
 
-<div class="container-fluid p-4 text-center">
+<div class="flex flex-column c-s-1 c-e-13 align-items-center">
 <h1>Page not found.</h1>
-<h1><?=$title?></h1>
+<img src="<?=ROOT?>/assets/images/404_cat.png" style="max-height:573px; max-width:510px;" alt="404">
+<a href="<?=ROOT?>"><h1>Go to Home</h1></a>
+
 </div>
 
 
