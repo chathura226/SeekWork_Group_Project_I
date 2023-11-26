4c6669a81caeb6d85ec5901ba59e5cfff2c62f32
chathura226 <chathuralakshan226@gmail.com>
2023-11-26 08:27:46 +0530

    message when logged out

--- app/controllers/Logout.php
+++ app/controllers/Logout.php
@@ -5,6 +5,7 @@ class Logout extends Controller{
 
     public function index(){
        Auth::logout();
+       message(['Logged Out Successfully!','success']);
        redirect('home');
     }
     
