6abe08f7c4f1c2d98b183a5631a79e6d95374f07
BKRavindu <bkravindu2000@gmai.com>
2023-12-05 20:58:48 +0530

    add signup page

---
+++ chat/.history/signup_20231205205556.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+
+    </head>
+    <style type="text/css">
+
+        @font-face {
+            font-family: headFont;
+            src: url(ui/fonts/Summer-Vibes-OTF.otf);
+        }
+
+        @font-face {
+            font-family: myfont;
+            src: url(ui/fonts/OpenSans-Regular.ttf);
+        }
+        
+
+        #wrapper{
+
+            max-width: 900px;
+            min-height: 500px;
+            margin:auto;
+            color:grey;
+            font-family:myfont;
+            font-size:13px;
+
+        }
+
+        form{
+            margin:auto;
+            padding:10px;
+            width:100%;
+            max-width: 400px;
+        }
+
+        input{
+            padding:10px;
+            margin:10px;
+            width:98%;
+            border-radius:5px;
+            border:solid 1px grey;
+        }
+
+        input[type=submit]{
+            width:104%;
+            background-color:pink;
+            cursor: pointer;
+        }
+
+        #header{
+            background-color: #485b6c;
+            height: auto;
+            font-size:40px;
+            text-align:center;
+            font-family:headFont;
+            width:100%;
+            color:white;
+
+
+        }
+
+
+    </style>
+    <body>
+        <div id="wrapper">
+
+        <div id="header">
+            My Chat
+
+            <div style="font-size:20px; font-family:myFont">Login</div>
+        
+        </div>
+
+            <form>
+                <input type="text"  name="username" placeholder="Username"><br>
+                <input type="password"  name="password" placeholder="Password"><br>
+                <input type="password"  name="password2" placeholder="Retype Password"><br>
+                <input type="submit"  value="Sign up"><br>
+            </form>
+
+
+        </div>
+    </body>
+</html>
+
+<script type="text/javascript">
+
+    function _(element){
+        return document.getElementById(element);
+    }
+
+    
+   
+    var label = _("label_chat");
+
+    label.addEventListener("click",function(){
+
+        var inner_pannel = _("inner_left_pannel");
+
+        var ajax = new XMLHttpRequest();
+
+        ajax.onload = function(){
+
+            if(ajax.status==200 || ajax.readyState==4){
+
+                inner_pannel.innerHTML = ajax.responseText;
+
+
+            }
+
+
+        }
+
+        ajax.open("POST","file.txt",true);
+        ajax.send();
+       
+
+    });
+
+
+
+</script>    
---
+++ chat/.history/ui/signup_20231205202833.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+
+    </head>
+    <style type="text/css">
+
+        @font-face {
+            font-family: headFont;
+            src: url(ui/fonts/Summer-Vibes-OTF.otf);
+        }
+
+        @font-face {
+            font-family: myfont;
+            src: url(ui/fonts/OpenSans-Regular.ttf);
+        }
+        
+
+        #wrapper{
+
+            max-width: 900px;
+            min-height: 500px;
+            display: flex;
+            margin:auto;
+            color:grey;
+            font-family:myfont;
+            font-size:13px;
+
+        }
+
+        form{
+            margin:auto;
+            padding:10px;
+            background-color:black;
+        }
+
+
+    </style>
+    <body>
+        <div id="wrapper">
+
+            <form>
+                <input type="text"  name="username" placeholder="Username"><br>
+                <input type="password"  name="password" placeholder="Password"><br>
+                <input type="password"  name="password2" placeholder="Retype Password"><br>
+                <input type="submit"  value="Sign up"><br>
+            </form>
+
+
+        </div>
+    </body>
+</html>
+
+<script type="text/javascript">
+
+    function _(element){
+        return document.getElementById(element);
+    }
+
+    
+   
+    var label = _("label_chat");
+
+    label.addEventListener("click",function(){
+
+        var inner_pannel = _("inner_left_pannel");
+
+        var ajax = new XMLHttpRequest();
+
+        ajax.onload = function(){
+
+            if(ajax.status==200 || ajax.readyState==4){
+
+                inner_pannel.innerHTML = ajax.responseText;
+
+
+            }
+
+
+        }
+
+        ajax.open("POST","file.txt",true);
+        ajax.send();
+       
+
+    });
+
+
+
+</script>    
