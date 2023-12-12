9e3f4a7a9c55c64f1e50761976c1f873d5311713
chathura226 <chathuralakshan226@gmail.com>
2023-12-12 06:44:27 +0530

    changed the task controller to send proposal data and view to showthe necessary proposal data

--- app/controllers/Tasks.php
+++ app/controllers/Tasks.php
@@ -37,6 +37,14 @@ class Tasks extends Controller{
                 // show($compDetails);
                 // die;
 
+                //if student is viewing, show them whether they have submitted any proposals
+                if(Auth::logged_in() && Auth::is_student()){
+                    $proposalInst=new Proposal();
+                    $row2=$proposalInst->first(['studentID'=>Auth::getstudentID(),'taskID'=>$id]);
+                    if(!empty($row2)){
+                        $data['proposal']=$row2; // to tell that they have already submitted a proposal for this
+                    }
+                }
                 if(!empty($compDetails)){
                     $data['error']="Error fetching data!";
                 }
--- app/views/task/task.view.php
+++ app/views/task/task.view.php
@@ -1,135 +1,138 @@
 <?php $this->view('includes/header', $data) ?>
 <?php $this->view('includes/nav', $data) ?>
 
-<link rel="stylesheet" href="<?= ROOT ?>/assets/css/eachtask.styles.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/eachtask.styles.css">
 
-<div class="pagetitle column-12">
-    <h1><?= $task->title ?></h1>
-    <nav>
+    <div class="pagetitle column-12">
+        <h1><?= $task->title ?></h1>
+        <nav>
 
-        <ul class="breadcrumbs">
-            <li class="breadcrumbs__item">
-                <a href="<?= ROOT ?>" class="breadcrumbs__link">Home</a>
-            </li>
-            <li class="breadcrumbs__item">
-                <a href="<?= ROOT ?>/tasks" class="breadcrumbs__link">Tasks</a>
-            </li>
-            <li class="breadcrumbs__item">
-                <a href="" class="breadcrumbs__link breadcrumbs__link--active"><?= $task->title ?></a>
-            </li>
-        </ul>
-    </nav>
-</div><!-- End Page Title -->
+            <ul class="breadcrumbs">
+                <li class="breadcrumbs__item">
+                    <a href="<?= ROOT ?>" class="breadcrumbs__link">Home</a>
+                </li>
+                <li class="breadcrumbs__item">
+                    <a href="<?= ROOT ?>/tasks" class="breadcrumbs__link">Tasks</a>
+                </li>
+                <li class="breadcrumbs__item">
+                    <a href="" class="breadcrumbs__link breadcrumbs__link--active"><?= $task->title ?></a>
+                </li>
+            </ul>
+        </nav>
+    </div><!-- End Page Title -->
 
-<div class="task-details-wrapper column-12">
+    <div class="task-details-wrapper column-12">
 
-    <div class="task-details column-7">
-        <div class="flex" style="justify-content:space-between;">
-            <h2>Task Details</h2>
+        <div class="task-details column-7">
+            <div class="flex" style="justify-content:space-between;">
+                <h2>Task Details</h2>
 
-            <button class="likeBtn">
+                <button class="likeBtn">
                 <span class="leftContainer">
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="#fff">
                         <path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"></path>
                     </svg>
                     <span class="like">Like</span>
                 </span>
-                <span class="likeCount">
+                    <span class="likeCount">
                     2,050
                 </span>
-            </button>
+                </button>
 
 
+            </div>
+            <div class="task-description">
+                <?= $task->description ?>
+                <br><br>
+                <h4>Relavant Documents : </h4>
+                <?php if (!empty($task->documents)) : ?>
+                    <?php $fileName = array_reverse(explode("/", $task->documents))[0]; ?>
+                    File Name: <?= $fileName ?><br>
+                    <a href="<?= ROOT ?>/download/tasks/<?= $task->taskID ?>/details?file=<?= $fileName ?>">Download</a>
+                    <br>
+                <?php else: ?>
+                    No files uploaded for this task!<br>
+                <?php endif; ?>
+                <br>
+                <h4>Relavant Tags : </h4>
+                <div class="tags-wrapper">
+
+                    <div class="tags">Logo</div>
+                    <div class="tags">Graphic</div>
+                    <div class="tags">Photoshop</div>
+                    <div class="tags">Illustrator</div>
+                    <div class="tags">Desiging</div>
+                    <div class="tags">Arwork</div>
+
+                </div>
+            </div>
+            <div class="about-task">
+                <h2>About the task</h2>
+
+                <div class="about-task-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 640 512">
+                        <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0l57.4-43c23.9-59.8 79.7-103.3 146.3-109.8l13.9-10.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176V384c0 35.3 28.7 64 64 64H360.2C335.1 417.6 320 378.5 320 336c0-5.6 .3-11.1 .8-16.6l-26.4 19.8zM640 336a144 144 0 1 0 -288 0 144 144 0 1 0 288 0zm-76.7-43.3c6.2 6.2 6.2 16.4 0 22.6l-72 72c-6.2 6.2-16.4 6.2-22.6 0l-40-40c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L480 353.4l60.7-60.7c6.2-6.2 16.4-6.2 22.6 0z"/>
+                    </svg>
+                    No. of proposals : 30</br>
+                </div>
+
+                <div class="about-task-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
+                        <path d="M176 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h16V98.4C92.3 113.8 16 200 16 304c0 114.9 93.1 208 208 208s208-93.1 208-208c0-41.8-12.3-80.7-33.5-113.2l24.1-24.1c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L355.7 143c-28.1-23-62.2-38.8-99.7-44.6V64h16c17.7 0 32-14.3 32-32s-14.3-32-32-32H224 176zm72 192V320c0 13.3-10.7 24-24 24s-24-10.7-24-24V192c0-13.3 10.7-24 24-24s24 10.7 24 24z"/>
+                    </svg>
+                    Deadline : 06/12/2023</br>
+                </div>
+                <div class="about-task-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512">
+                        <path d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                    </svg>
+                    Task Type : <?= ucfirst($task->taskType) ?></br>
+                </div>
+
+            </div>
         </div>
-        <div class="task-description">
-            <?= $task->description ?>
-            <br><br>
-            <h4>Relavant Documents : </h4>
-            <?php if (!empty($task->documents)) : ?>
-                <?php $fileName=array_reverse(explode("/",$task->documents))[0];?>
-                File Name: <?=$fileName?><br>
-                <a href="<?=ROOT?>/download/tasks/<?=$task->taskID?>/details?file=<?=$fileName?>">Download</a><br>
-            <?php else:?>
-                No files uploaded for this task!<br>
+        <div class="price-button c-s-8 c-e-13">
+            <?php if (!empty($proposal)): ?>
+                <span style="color: red">You have already submitted a proposal with the value : Rs. <?= (!empty($proposal->proposeAmount)) ? $proposal->proposeAmount : $task->value; ?></span>
             <?php endif; ?>
-            <br>
-            <h4>Relavant Tags : </h4>
-            <div class="tags-wrapper">
-
-                <div class="tags">Logo</div>
-                <div class="tags">Graphic</div>
-                <div class="tags">Photoshop</div>
-                <div class="tags">Illustrator</div>
-                <div class="tags">Desiging</div>
-                <div class="tags">Arwork</div>
-
-            </div>
-        </div>
-        <div class="about-task">
-            <h2>About the task</h2>
-
-            <div class="about-task-item">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 640 512">
-                    <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0l57.4-43c23.9-59.8 79.7-103.3 146.3-109.8l13.9-10.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176V384c0 35.3 28.7 64 64 64H360.2C335.1 417.6 320 378.5 320 336c0-5.6 .3-11.1 .8-16.6l-26.4 19.8zM640 336a144 144 0 1 0 -288 0 144 144 0 1 0 288 0zm-76.7-43.3c6.2 6.2 6.2 16.4 0 22.6l-72 72c-6.2 6.2-16.4 6.2-22.6 0l-40-40c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L480 353.4l60.7-60.7c6.2-6.2 16.4-6.2 22.6 0z" />
-                </svg>
-                No. of proposals : 30</br>
-            </div>
-
-            <div class="about-task-item">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
-                    <path d="M176 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h16V98.4C92.3 113.8 16 200 16 304c0 114.9 93.1 208 208 208s208-93.1 208-208c0-41.8-12.3-80.7-33.5-113.2l24.1-24.1c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L355.7 143c-28.1-23-62.2-38.8-99.7-44.6V64h16c17.7 0 32-14.3 32-32s-14.3-32-32-32H224 176zm72 192V320c0 13.3-10.7 24-24 24s-24-10.7-24-24V192c0-13.3 10.7-24 24-24s24 10.7 24 24z" />
-                </svg>
-                Deadline : 06/12/2023</br>
-            </div>
-            <div class="about-task-item">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512">
-                    <path d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-                </svg>
-                Task Type : <?= ucfirst($task->taskType) ?></br>
-            </div>
+            <h1>Rs.<?= $task->value ?>/=</h1>
+            <a href="<?= ROOT ?>/tasks/apply/<?= $task->taskID ?>">
+                <button class="apply">Apply</button>
+            </a>
 
         </div>
-    </div>
-    <div class="price-button c-s-8 c-e-13">
+        <div class="comp-details c-s-8 c-e-13">
+            <h2>About the company</h2>
+            <div class="comp-img">
+                <img src="<?= ROOT ?><?= (!empty($company->profilePic)) ? "/" . $company->profilePic : "/assets/images/noImage.png" ?>"
+                     alt="Profile Picture">
+            </div>
+            <h3><?= ucfirst($company->companyName) ?> <small>(2000 tasks)</small></h3>
+            <div class="rating">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                    <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                    <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                    <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                    <path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                    <path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/>
+                </svg>
+            </div>
+            Company Description : <?= $company->description ?></br>
+            Location : <?= ucfirst($company->address) ?></br>
+            <?php if (!empty($company->website)) echo "Website : " . $company->website; ?></br>
+            Joined Date: <?= $company->createdAt ?></br>
 
-        <h1>Rs.<?= $task->value ?>/=</h1>
-        <a href="<?= ROOT ?>/tasks/apply/<?= $task->taskID ?>"><button class="apply">Apply</button></a>
+        </div>
 
     </div>
-    <div class="comp-details c-s-8 c-e-13">
-        <h2>About the company</h2>
-        <div class="comp-img">
-            <img src="<?= ROOT ?><?= (!empty($company->profilePic)) ? "/" . $company->profilePic : "/assets/images/noImage.png" ?>" alt="Profile Picture">
-        </div>
-        <h3><?= ucfirst($company->companyName) ?> <small>(2000 tasks)</small></h3>
-        <div class="rating">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-            </svg><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-            </svg><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                <path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                <path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z" />
-            </svg>
-        </div>
-        Company Description : <?= $company->description ?></br>
-        Location : <?= ucfirst($company->address) ?></br>
-        <?php if (!empty($company->website)) echo "Website : " . $company->website; ?></br>
-        Joined Date: <?= $company->createdAt ?></br>
-
-    </div>
-
-</div>
-
-
-
-
-
 
 
 <?php $this->view("includes/footer", $data);
