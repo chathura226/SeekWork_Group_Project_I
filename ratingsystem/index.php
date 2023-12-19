28633589886880f60a73b650beb4ab310602c20b
BKRavindu <bkravindu2000@gmai.com>
2023-12-19 08:58:41 +0530

    create database for rating system and add index.php

---
+++ ratingsystem/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Reviews System</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+    <body>
+
+    </body>
+
+</html>    
