e0ab5b1b6000baedd6eeae086aee90224f961aa8
chathura226 <chathuralakshan226@gmail.com>
2023-12-28 09:45:47 +0530

    task submission email content

--- app/models/MailService.php
+++ app/models/MailService.php
@@ -173,7 +173,7 @@ class MailService
 
     }
 
-    public static function prepareNewInvitationEmaik($fullName,$task,$proposal)
+    public static function prepareNewInvitationEmaik($fullName, $task, $proposal)
     {
         $email = '<!DOCTYPE html>
             <html>
@@ -309,17 +309,149 @@ class MailService
         return $email;
 
     }
+
     //TODO: email structures
 
-    public static function prepareNewSubmissionEmail()
+    public static function prepareNewSubmissionEmail($fullName,$task,$submission)
     {
+        $email = '<!DOCTYPE html>
+                <html>
+                
+                <head>
+                    <meta charset="utf-8">
+                    <title>Email Template</title>
+                    <style>
+                        body {
+                            margin: 0;
+                            padding: 0;
+                            background-color: #f4f4f4;
+                            font-family: Arial, sans-serif;
+                        }
+                        .header {
+                            background: black;
+                            color: white;
+                
+                            padding: 40px 60px;
+                        }
+                        .container {
+                            max-width: 600px;
+                            margin: 0 auto;
+                            padding: 0;
+                            background: black;
+                            color: white;
+                        }
+                
+                        .content {
+                            padding: 100px;
+                            font-family: Inter, sans-serif;
+                        }
+                
+                
+                        .otp-section {
+                            padding: 20px 60px;
+                
+                
+                        }
+                
+                        .otp-code {
+                            background-color: #141414;
+                            color: #fac337;
+                            font-size: 20px;
+                            font-weight: 600;
+                            border-radius: 24px;
+                            padding: 15px 20px;
+                            margin: 20px 0;
+                            text-align:center;
+                        }
+                
+                        .info {
+                
+                            padding: 20px 60px;
+                            color: #757575;
+                        }
+                
+                        .footer {
+                            padding: 60px;
+                            color: #757575;
+                        }
+                
+                        .footer hr {
+                            border: 1px solid #333333;
+                            margin: 0;
+                        }
+                    </style>
+                </head>
+                
+                <body>
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td align="center">
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0" class="container">
+                                <tr>
+                                    <td class="header">
+                                        <img src="https://bit.ly/46vTlCU" alt="Logo">
+                                        <div style="font-size: 50px; font-weight: 600; margin-top: 20px;">New submission for your task </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="otp-section">
+                                        Hi ' . $fullName . ',<br><br>
+                                        <div style="font-size: 14px; font-weight: 500; margin-bottom: 20px;">You have a new submission for your ongoing task </div>
+                                        <h3><u>Task Summary</u></h3>
+                                        <ul>
+                                            <li>
+                                                <h4>Name of the task: <small>' . $task->title . '</small> </h4>
+                                            </li>
+                                            <li>
+                                                <h4>Submission Date: <small>' . $submission->createdAt . '</small> </h4>
+                                            </li>
+                
+                                        </ul>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="info">
+                                        <div style="font-size: 14px; font-weight: 500; margin-bottom: 10px;">You can also use the
+                                            below button to navigate to your tasks submissions page.</div>
+                                        <br>
+                                        <div style="background: white; border-radius: 32px; padding: 10px 20px; display: inline-block;">
+                                            <a href=' . '"' . ROOT . '/company/tasks/' . $task->taskID . '/submissions"' . ' style="text-decoration: none; color: black; font-weight: 600;">Check the Invitation</a>
+                                        </div>
+                                        <div style="font-size: 14px; font-weight: 500; margin-top: 20px;">If you have already taken necessary actions, you can safely ignore this email.</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="footer">
+                                        <hr>
+                                        <div style="font-size: 20px; font-weight: 600; margin: 20px 0;">SeekWork</div>
+                                        <div>Your platform for academic and professional success!</div>
+                                        <div style="margin-top: 10px;">
+                                            <a href="#" style="text-decoration: none; color: white; font-weight: 500; margin-right: 20px;">Help</a>
+                                            <a href="#" style="text-decoration: none; color: white; font-weight: 500;">Unsubscribe</a>
+                                        </div>
+                                        <div style="margin-top: 10px;">
+                                            <div>SeekWork</div>
+                                            <div>Sri Lanka</div>
+                                            <div>©2024 All rights reserved.</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                </body>
+                
+                </html>';
+
+        return $email;
 
 
     }
 
-    public static function prepareNewInvitationAcceptanceEmail($fullName,$assignment,$proposal,$task)
+    public static function prepareNewInvitationAcceptanceEmail($fullName, $assignment, $proposal, $task)
     {
-        $email='<!DOCTYPE html>
+        $email = '<!DOCTYPE html>
                 <html>
                 
                 <head>
