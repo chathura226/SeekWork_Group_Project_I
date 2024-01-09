a48980ee0c1aeed96f81de3142da3d8380b95317
chathura226 <chathuralakshan226@gmail.com>
2024-01-10 04:53:48 +0530

    changes for some bugs and ipg

--- app/views/aboutus.view.php
+++ app/views/aboutus.view.php
@@ -1,81 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Directpay|OneTimePayment</title>
+</head>
+<div id="card_container"></div>
+<body>
+<script src="https://cdn.directpay.lk/dev/v1/directpayCardPayment.js?v=1"></script>
 
-<?php
-$amount=3000;
-$merchant_id="1224435";
-$order_id=uniqid();
-$merchant_secret="MzY1NTkxMzIwMTQ4NDU3NzM5MTQwNTU0MjI0MTIxODQ2NjQ0NTU1";
-$currency="LKR";
-$hash = strtoupper(
-    md5(
-        $merchant_id . 
-        $order_id . 
-        number_format($amount, 2, '.', '') . 
-        $currency .  
-        strtoupper(md5($merchant_secret)) 
-    ) 
-);
-
-$array=[];
-$array["amount"]=$amount;
-$array["merchant_id"]=$merchant_id;
-$array["order_id"]=$order_id;
-$array["currency"]=$currency;
-$array["hash"]=$hash;
-
-$jsonObj=json_encode($array);
-echo $jsonObj;
-?>
-
-<script type="text/javascript" src="https://www.payhere.lk/lib/payhere.js"></script>
-<button type="submit" id="payhere-payment" >PayHere Pay</button>
 <script>
-    // Payment completed. It can be a successful failure.
-    payhere.onCompleted = function onCompleted(orderId) {
-        console.log("Payment completed. OrderID:" + orderId);
-        // Note: validate the payment and show success or failure page to the customer
-    };
+    DirectPayCardPayment.init({
+        container: 'card_container', //<div id="card_container"></div>
+        merchantId: 'ES15612', //your merchant_id
+        amount: "1000.00",
+        refCode: "hvy6ng6", //unique referance code form merchant
+        currency: 'LKR',
+        type: 'ONE_TIME_PAYMENT',
+        customerEmail: 'abc@mail.com',
+        customerMobile: '+94712345674',
+        description: '154 products',  //product or service description
+        debug: true,
+        responseCallback: responseCallback,
+        errorCallback: errorCallback,
+        logo: 'https://test.com/directpay_logo.png',
+        apiKey: '013b6dc745f667a0b91aeada206eccf87e0449fb92dada480e79f7045bf40101'
+    });
 
-    // Payment window closed
-    payhere.onDismissed = function onDismissed() {
-        // Note: Prompt user to pay again or show an error page
-        console.log("Payment dismissed");
-    };
+    //response callback.
+    function responseCallback(result) {
+        console.log("successCallback-Client", result);
+        alert(JSON.stringify(result));
+    }
 
-    // Error occurred
-    payhere.onError = function onError(error) {
-        // Note: show an error page
-        console.log("Error:"  + error);
-    };
-
-    // Put the payment variables here
-    var payment = {
-        "sandbox": true,
-        "merchant_id": "<?=$merchant_id?>",    // Replace your Merchant ID
-        "return_url": "<?=ROOT?>/assets/js/payhere.js",     // Important
-        "cancel_url": undefined,     // Important
-        "notify_url": "http://sample.com/notify",
-        "order_id": "<?=$order_id?>",
-        "items": "Door bell wireles",
-        "amount": "<?=$amount?>",
-        "currency": "<?=$currency?>",
-        "hash": "<?=$hash?>", // *Replace with generated hash retrieved from backend
-        "first_name": "Saman",
-        "last_name": "Perera",
-        "email": "samanp@gmail.com",
-        "phone": "0771234567",
-        "address": "No.1, Galle Road",
-        "city": "Colombo",
-        "country": "Sri Lanka",
-        "delivery_address": "No. 46, Galle road, Kalutara South",
-        "delivery_city": "Kalutara",
-        "delivery_country": "Sri Lanka",
-        "custom_1": "",
-        "custom_2": ""
-    };
-
-    // Show the payhere.js popup, when "PayHere Pay" is clicked
-    document.getElementById('payhere-payment').onclick = function (e) {
-        payhere.startPayment(payment);
-    };
+    //error callback
+    function errorCallback(result) {
+        console.log("successCallback-Client", result);
+        alert(JSON.stringify(result));
+    }
 </script>
-
+</body>
+</html>
