0417eea3203fa7e494ee229c4882e8086521ed39
chathura226 <chathuralakshan226@gmail.com>
2024-01-26 17:45:08 +0530

    basic notification drop down css and html

--- app/views/student/student-header.view.php
+++ app/views/student/student-header.view.php
@@ -23,6 +23,7 @@
 
 
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/admin-header.styles.css" />
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/notification.styles.css" />
     <!-- <link href="<?= ROOT ?>/assets/css/nav.styles.css" rel="stylesheet"> this is included in the student-header style.css file with modifications -->
 </head>
 
@@ -61,11 +62,22 @@
                     <div class="profile-pic">
                         <img src="<?= ROOT ?><?= (!empty(Auth::getprofilePic())) ? "/" . Auth::getprofilePic() : "/assets/images/noImage.png" ?>" alt="Profile Picture">
                     </div>
-                    <button class="notification">
+
+
+
+
+                    <button class="notification" onclick="notificationClick(event)" id="notifyBttn">
                         <svg viewBox="0 0 448 512" class="bell">
                             <path d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"></path>
                         </svg>
                     </button>
+                    <div style="position: absolute; top:100%;" class="notif-dropdown-content" id="notificationContent">
+                        <a href="#">Designing</a>
+                        <a  href="#">Web Development</a>
+                        <a href="#">Programming</a>
+                    </div>
+
+
                     <a href="<?= ROOT ?>/logout"><button class="login-button"> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="svg-icons">
                                 <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
                             </svg><span>Logout</span></button></a>
@@ -202,3 +214,30 @@
                     <h3><?= message([], true)[0] ?></h3>
                 </div>
             <?php endif; ?>
+
+
+
+            <script>
+                notificationContent=document.getElementById("notificationContent");
+                notifyBttn=document.getElementById("notifyBttn");
+                var isNotificationOpen=false;
+                //to open notification drop
+                function notificationClick(e){
+                    e.preventDefault();
+                    if(isNotificationOpen){
+                        notificationContent.style.display = "none";
+                        isNotificationOpen = false;
+                    }else {
+                        notificationContent.style.display = "block";
+                        isNotificationOpen = true;
+                    }
+                }
+
+                //to track other clicks to close notification drop
+                document.addEventListener('click', (event) => {
+                    if (!notificationContent.contains(event.target) && !notifyBttn.contains(event.target)) {
+                        console.log("erd");
+                        notificationClick(event);
+                    }
+                });
+            </script>
