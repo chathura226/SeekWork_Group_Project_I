f3cecd8e87539a7ce0d66c4c3153a1c13e5a3805
chathura226 <chathuralakshan226@gmail.com>
2024-02-01 09:51:57 +0530

    task progress chart controller for ajax

--- app/views/student/dashboard.view.php
+++ app/views/student/dashboard.view.php
@@ -86,6 +86,35 @@
                     datasets: [{
                         label: data.label,
                         data: data.data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 205, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(0, 255, 255, 0.2)',
+                            'rgba(201, 203, 207, 0.2)',
+                            'rgba(255, 0, 0, 0.2)',
+                            'rgba(0, 255, 0, 0.2)',
+                            'rgba(0, 0, 255, 0.2)',
+                            'rgba(128, 0, 128, 0.2)' // Purple color added as an alternative
+                        ],
+                        borderColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(255, 159, 64)',
+                            'rgb(255, 205, 86)',
+                            'rgb(75, 192, 192)',
+                            'rgb(54, 162, 235)',
+                            'rgb(153, 102, 255)',
+                            'rgb(255, 255, 0)',
+                            'rgb(201, 203, 207)',
+                            'rgb(255, 0, 0)',
+                            'rgb(0, 255, 0)',
+                            'rgb(0, 0, 255)',
+                            'rgb(128, 0, 128)' // Purple color added as an alternative
+                        ],
+
                         borderWidth: 1
                     }]
                 },
