e8f472138946375b3b1ec8589bd1220b5d0f14dd
chathura226 <chathuralakshan226@gmail.com>
2024-02-06 14:15:30 +0530

    support contact us page finished

--- app/controllers/Support.php
+++ app/controllers/Support.php
@@ -9,5 +9,14 @@ class Support extends Controller{
         
         $this->view('support',$data);
     }
+    public function help(){
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            show($_POST);die;
+        }
+        $data['title'] = "Contact Us";
+
+        $this->view('contactus',$data);
+    }
     
 }
