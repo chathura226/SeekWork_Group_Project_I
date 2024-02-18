8a088b9424de40cb09940f97e032efa63a678c8a
chathura226 <chathuralakshan226@gmail.com>
2024-02-18 21:17:59 +0530

    rempved old search and added new search and sort

--- app/views/student/proposals.view.php
+++ app/views/student/proposals.view.php
@@ -24,14 +24,6 @@
 
         <input class="input-search-new" id="search-bar" name="search-bar" placeholder="Search..." type="search">
     </div>
-<!--  <div class="search-group c-s-2 c-e-13 r-s-1 r-e-2">-->
-<!--    <svg class="icon-search" aria-hidden="true" viewBox="0 0 24 24" style="padding: 0 !important;">-->
-<!--      <g>-->
-<!--        <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>-->
-<!--      </g>-->
-<!--    </svg>-->
-<!--    <input placeholder="Search" type="search" class="input-search" id="search-bar">-->
-<!--  </div>-->
 
 
   <h1>Proposals</h1>
@@ -90,6 +82,7 @@
 
 
 <script type="text/javascript" src="<?= ROOT ?>/assets/js/searchNearBreadCrums.js"></script>
+<!--js for sorting-->
 <script>
     let AscDate=0;//for toggling sorting direction of date
     let AscValue=0;//for toggling sorting direction of value
@@ -114,7 +107,6 @@
                 // Extract the date text from the divs
                 const dateTextA = a.getElementsByTagName('h4')[0].textContent.trim();
                 const dateTextB = b.getElementsByTagName('h4')[0].textContent.trim();
-
                 // Convert date text to Date objects for comparison
                 const dateA = new Date(dateTextA);
                 const dateB = new Date(dateTextB);
