f740521f51d29255a05be77eacda89609eb5bcc9
chathura226 <chathuralakshan226@gmail.com>
2024-02-19 11:19:14 +0530

    search and filter for pending invites in student

--- app/views/student/pendinginvites.view.php
+++ app/views/student/pendinginvites.view.php
@@ -1,12 +1,13 @@
-<?php $this->view('student/student-header',$data) ?>
+<?php $this->view('student/student-header', $data) ?>
 
 
-<link rel="stylesheet" href="<?=ROOT?>/assets/css/reviews.styles.css"/>
-<link rel="stylesheet" href="<?=ROOT?>/assets/css/floating-button.styles.css"/>
+<link rel="stylesheet" href="<?= ROOT ?>/assets/css/reviews.styles.css"/>
+<link rel="stylesheet" href="<?= ROOT ?>/assets/css/floating-button.styles.css"/>
 
 
 <div class="pagetitle column-12">
-    <div class="searchAndFilter" style="position:absolute;right: 30px;top: 17px;display: flex;flex-wrap: wrap;gap: 15px">
+    <div class="searchAndFilter"
+         style="position:absolute;right: 30px;top: 17px;display: flex;flex-wrap: wrap;gap: 15px">
 
         <label class="dropdown">Sort By
 
@@ -26,122 +27,124 @@
 
         <input class="input-search-new" id="search-bar" name="search-bar" placeholder="Search..." type="search">
     </div>
-      <h1>Pending Assignment Invitations</h1>
-      <nav>
+    <h1>Pending Assignment Invitations</h1>
+    <nav>
 
         <ul class="breadcrumbs">
-          <li class="breadcrumbs__item">
-            <a href="<?=ROOT?>" class="breadcrumbs__link">Home</a>
-          </li>
-          <li class="breadcrumbs__item">
-            <a href="<?=ROOT?>/<?=Auth::getrole()?>" class="breadcrumbs__link">Dashboard</a>
-          </li>
-          <li class="breadcrumbs__item">
-            <a href="" class="breadcrumbs__link breadcrumbs__link--active">Pending Invites</a>
-          </li>
+            <li class="breadcrumbs__item">
+                <a href="<?= ROOT ?>" class="breadcrumbs__link">Home</a>
+            </li>
+            <li class="breadcrumbs__item">
+                <a href="<?= ROOT ?>/<?= Auth::getrole() ?>" class="breadcrumbs__link">Dashboard</a>
+            </li>
+            <li class="breadcrumbs__item">
+                <a href="" class="breadcrumbs__link breadcrumbs__link--active">Pending Invites</a>
+            </li>
         </ul>
-      </nav>
+    </nav>
 </div><!-- End Page Title -->
 
 
 <div class="myreviews-wrapper column-12">
-  <?php if(!empty($assignments)):?>
-  <?php foreach($assignments as $assignment):?>
-  <div class="myreview-review-bttns">
-  <div class="myreview-review">
-    <h2>For Task : <?=ucfirst($assignment->task->title)?></h2>
-    <h4>Proposal Submitted Date: <?=ucfirst($assignment->proposal->submissionDate)?></h4>
-    <h4>Proposal Value: <?=(!empty($assignment->proposal->proposeAmount))?ucfirst($assignment->proposal->proposeAmount):ucfirst($assignment->task->value)?></h4>
-    <h4>Assignment Invitation Date: <?=ucfirst($assignment->createdAt)?></h4>
-    <h4>By Company:  <?=ucfirst($assignment->company->companyName)?></h4>
-    
+    <?php if (!empty($assignments)): ?>
+        <?php foreach ($assignments as $assignment): ?>
+            <div class="myreview-review-bttns">
+                <div class="myreview-review">
+                    <h2>For Task : <span> <?= ucfirst($assignment->task->title) ?></span></h2>
+                    <h4>Proposal Submitted Date: <?= ucfirst($assignment->proposal->submissionDate) ?></h4>
+                    <h4>Proposal
+                        Value: Rs.
+                        <span><?= (!empty($assignment->proposal->proposeAmount)) ? ucfirst($assignment->proposal->proposeAmount) : ucfirst($assignment->task->value) ?></span>
+                    </h4>
+                    <h4>Assignment Invitation Date: <?= ucfirst($assignment->createdAt) ?></h4>
+                    <h4>By Company: <?= ucfirst($assignment->company->companyName) ?></h4>
 
-    </div>
-  <div class="bttn-wrapper-new">
-  <a href="<?=ROOT?>/tasks/<?=$assignment->task->taskID?>">
-    <button class="details-button bigbttn">
-      View the task
-      <div class="arrow-wrapper">
-          <div class="arrow"></div>
-      </div>
-    </button>
-  </a>
-  <a href="<?=ROOT?>/student/proposals/<?=$assignment->proposal->proposalID?>">
-    <button class="details-button bigbttn">
-      View the Proposal
-      <div class="arrow-wrapper">
-          <div class="arrow"></div>
-      </div>
-    </button>
-  </a>
-        <button class="details-button margin-5 accbtn deletebttn"  data-id="<?=$assignment->assignmentID?>">
-        Accept
-        <div class="arrow-wrapper">
-            <div class="arrow"></div>
-        </div>
-      </button>
-      <button class="details-button margin-5 decbtn red deletebttn"  data-id="<?=$assignment->assignmentID?>">
-        Decline
-        <div class="arrow-wrapper">
-            <div class="arrow"></div>
-        </div>
-      </button>
 
-    </div>
-    </div>
+                </div>
+                <div class="bttn-wrapper-new">
+                    <a href="<?= ROOT ?>/tasks/<?= $assignment->task->taskID ?>">
+                        <button class="details-button bigbttn">
+                            View the task
+                            <div class="arrow-wrapper">
+                                <div class="arrow"></div>
+                            </div>
+                        </button>
+                    </a>
+                    <a href="<?= ROOT ?>/student/proposals/<?= $assignment->proposal->proposalID ?>">
+                        <button class="details-button bigbttn">
+                            View the Proposal
+                            <div class="arrow-wrapper">
+                                <div class="arrow"></div>
+                            </div>
+                        </button>
+                    </a>
+                    <button class="details-button margin-5 accbtn deletebttn"
+                            data-id="<?= $assignment->assignmentID ?>">
+                        Accept
+                        <div class="arrow-wrapper">
+                            <div class="arrow"></div>
+                        </div>
+                    </button>
+                    <button style="background-color: red" class="details-button margin-5 decbtn red deletebttn"
+                            data-id="<?= $assignment->assignmentID ?>">
+                        Decline
+                        <div class="arrow-wrapper">
+                            <div class="arrow" style="background-color: red"></div>
+                        </div>
+                    </button>
 
-  <?php endforeach;?>
-<?php endif;?>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+    <?php endif; ?>
 </div>
 
 
-
-
-<script >
+<script>
     // Get all accept buttons
-const accbttns = document.querySelectorAll('.details-button.margin-5.accbtn.deletebttn');
+    const accbttns = document.querySelectorAll('.details-button.margin-5.accbtn.deletebttn');
 
-// Attach a click event listener to each button
-accbttns.forEach(button => {
-    button.addEventListener('click', function(event) {
-        // Get the data-id attribute value
-        const itemId = event.currentTarget.getAttribute('data-id');
-        const confirmDelete = confirm("Are you sure you want to Accept the invitation?");
+    // Attach a click event listener to each button
+    accbttns.forEach(button => {
+        button.addEventListener('click', function (event) {
+            // Get the data-id attribute value
+            const itemId = event.currentTarget.getAttribute('data-id');
+            const confirmDelete = confirm("Are you sure you want to Accept the invitation?");
 
-        if (confirmDelete) {
-            const action = "accept"; // Define the action here
-            // Send the action to the current URL
-            sendActionToCurrentURL(action,itemId);
-        } else {
-            alert("Canceled!");
-        }
+            if (confirmDelete) {
+                const action = "accept"; // Define the action here
+                // Send the action to the current URL
+                sendActionToCurrentURL(action, itemId);
+            } else {
+                alert("Canceled!");
+            }
+        });
     });
-});
 
 
     // Get all decline buttons
     const decbtns = document.querySelectorAll('.details-button.margin-5.decbtn.red.deletebttn');
 
-// Attach a click event listener to each button
-decbtns.forEach(button => {
-    button.addEventListener('click', function(event) {
-        // Get the data-id attribute value
-        const itemId = event.currentTarget.getAttribute('data-id');
-        const confirmDelete = confirm("Are you sure you want to Decline the invitation?");
+    // Attach a click event listener to each button
+    decbtns.forEach(button => {
+        button.addEventListener('click', function (event) {
+            // Get the data-id attribute value
+            const itemId = event.currentTarget.getAttribute('data-id');
+            const confirmDelete = confirm("Are you sure you want to Decline the invitation?");
 
-        if (confirmDelete) {
-            const action = "decline"; // Define the action here
-            // Send the action to the current URL
-            sendActionToCurrentURL(action,itemId);
-        } else {
-            alert("Canceled!");
-        }
+            if (confirmDelete) {
+                const action = "decline"; // Define the action here
+                // Send the action to the current URL
+                sendActionToCurrentURL(action, itemId);
+            } else {
+                alert("Canceled!");
+            }
+        });
     });
-});
 
 
-
-function sendActionToCurrentURL(action,id) {
+    function sendActionToCurrentURL(action, id) {
         // Create a form dynamically
         const form = document.createElement("form");
         form.method = "POST";
@@ -163,23 +166,39 @@ function sendActionToCurrentURL(action,id) {
         // Submit the form
         form.submit();
         console.log(form.action);
-        
+
     }
 </script>
 
 
-<?php $this->view('student/student-footer',$data) ?>
+<?php $this->view('student/student-footer', $data) ?>
 
-<script type="text/javascript" src="<?= ROOT ?>/assets/js/searchNearBreadCrums.js"></script>
+<!--//script for search-->
+<script>
+    document.getElementById('search-bar').addEventListener('input', function () {
+        let filter = this.value.toLowerCase();
+        let items = document.getElementsByClassName("myreview-review-bttns");
+
+        for (let i = 0; i < items.length; i++) {
+            let itemName = items[i].querySelectorAll('.myreview-review h2 span')[0].textContent.toLowerCase();
+            if (itemName.indexOf(filter) > -1) {
+                items[i].style.display = 'flex';
+            } else {
+                items[i].style.display = 'none';
+            }
+        }
+    });
+</script>
+
 <!--js for sorting-->
 <script>
-    let AscTitle=0;
-    let AscDate=0;//for toggling sorting direction of date
-    let AscValue=0;//for toggling sorting direction of value
-    function sortByFilter(feature){
+    let AscTitle = 0;
+    let AscDate = 0;//for toggling sorting direction of date
+    let AscValue = 0;//for toggling sorting direction of value
+    function sortByFilter(feature) {
         let str = feature.toLowerCase();
 
-        document.getElementById('sortByTitle').textContent=str.charAt(0).toUpperCase() + str.slice(1);
+        document.getElementById('sortByTitle').textContent = str.charAt(0).toUpperCase() + str.slice(1);
 
         let items = document.getElementsByClassName("myreview-review-bttns");
 
@@ -187,7 +206,7 @@ function sendActionToCurrentURL(action,id) {
         let divsArray = Array.from(items);
 
         //for date sorting
-        if(str==='date') {
+        if (str === 'date') {
             //togle the asc desc for date
             if (AscDate === 0) AscDate = 1;
             else AscDate = 0;
@@ -196,8 +215,9 @@ function sendActionToCurrentURL(action,id) {
             // Sort the divs based on their date text
             divsArray.sort((a, b) => {
                 // Extract the date text from the divs
-                const dateTextA = a.getElementsByTagName('h4')[0].textContent.trim();
-                const dateTextB = b.getElementsByTagName('h4')[0].textContent.trim();
+                const dateTextA = a.querySelectorAll('.myreview-review h4')[2].textContent.trim();
+                const dateTextB = b.querySelectorAll('.myreview-review h4')[2].textContent.trim();
+                // console.log(dateTextA)
                 // Convert date text to Date objects for comparison
                 const dateA = new Date(dateTextA);
                 const dateB = new Date(dateTextB);
@@ -209,7 +229,7 @@ function sendActionToCurrentURL(action,id) {
                     return dateB - dateA;
                 }
             });
-        }else if(str==='value'){
+        } else if (str === 'value') {
             //togle the asc desc for value
             if (AscValue === 0) AscValue = 1;
             else AscValue = 0;
@@ -217,9 +237,9 @@ function sendActionToCurrentURL(action,id) {
             // Sort the divs based on their date text
             divsArray.sort((a, b) => {
                 // Extract the date text from the divs
-                const valueA = a.querySelector('h4 span').textContent.trim();
-                const valueB = b.querySelector('h4 span').textContent.trim();
-
+                const valueA = a.querySelector('.myreview-review h4:nth-of-type(2) span').textContent.trim();
+                const valueB = b.querySelector('.myreview-review h4:nth-of-type(2) span').textContent.trim();
+                console.log(valueA, valueB)
                 // Convert values text to float for comparison
                 const valA = parseFloat(valueA);
                 const valB = parseFloat(valueB);
@@ -231,7 +251,7 @@ function sendActionToCurrentURL(action,id) {
                     return valB - valA;
                 }
             });
-        }else if(str==='title'){
+        } else if (str === 'title') {
             //togle the asc desc for value
             if (AscTitle === 0) AscTitle = 1;
             else AscTitle = 0;
@@ -239,17 +259,17 @@ function sendActionToCurrentURL(action,id) {
             // Sort the divs based on their date text
             divsArray.sort((a, b) => {
                 // Extract the date text from the divs
-                const textA = a.querySelector('h2').textContent.trim().toLowerCase();
-                const textB = b.querySelector('h2').textContent.trim().toLowerCase();
-
-                if (textA < textB) return (AscTitle === 0)?-1:1;
-                if (textA > textB) return (AscTitle === 0)?1:-1;
+                const textA = a.querySelectorAll('.myreview-review h2 span')[0].textContent.trim().toLowerCase();
+                const textB = b.querySelectorAll('.myreview-review h2 span')[0].textContent.trim().toLowerCase();
+                console.log(textA,textB)
+                if (textA < textB) return (AscTitle === 0) ? -1 : 1;
+                if (textA > textB) return (AscTitle === 0) ? 1 : -1;
                 return 0;
             });
         }
 
         // Clear the container before appending sorted divs
-        const container = document.querySelector('.mytasks-wrapper');
+        const container = document.querySelector('.myreviews-wrapper.column-12');
         container.innerHTML = '';
 
         // Append sorted divs back to the container
