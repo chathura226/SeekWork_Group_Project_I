5e682920ee43eb3601c227e0ce8b48d0b4720881
chathura226 <chathuralakshan226@gmail.com>
2024-02-27 10:07:44 +0530

    login logout links according to the cureent state

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -241,9 +241,16 @@
             <span id="close-btn"><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></span>
             <li><a href="<?= ROOT ?>">Home</a></li>
             <li><a href="<?= ROOT ?>/tasks"">Explore</a></li>
-            <li><a href="<?= ROOT ?>/signup">Signup as a Company</a></li>
-            <li><a href="<?= ROOT ?>/login">Login</a></li>
-            <li><a href="<?= ROOT ?>/signup">Sign up</a></li>
+            <li><a href="<?= ROOT ?>/about"">About us</a></li>
+            <li><a href="<?= ROOT ?>/support"">Help & Support</a></li>
+            <?php if (!Auth::logged_in()) : ?>
+                <li><a href="<?= ROOT ?>/signup">Signup as a Company</a></li>
+                <li><a href="<?= ROOT ?>/login">Login</a></li>
+                <li><a href="<?= ROOT ?>/signup">Sign up</a></li>
+            <?php else : ?>
+                <li><a href="<?= ROOT ?>/logout">Logout</a></li>
+            <?php endif; ?>
+
         </ul>
 
         <span id="hamburger-btn"><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></span>
@@ -408,19 +415,19 @@
 <!--background image transition-->
 <script>
     let headerColor="#000";
-
-    //add color to header when scroll
-    window.addEventListener('scroll', function() {
+    changeheaderColor= function() {
         var header = document.querySelector('header');
         var scrollPosition = window.scrollY;
 
         // Add class to header when scroll position is greater than 100px
-        if (scrollPosition > 100) {
+        if (scrollPosition > 150) {
             header.style.backgroundColor=headerColor;
         } else {
             header.style.backgroundColor='transparent';
         }
-    });
+    }
+    //add color to header when scroll
+    window.addEventListener('scroll', changeheaderColor);
 
 
     // Array of background images
@@ -432,6 +439,16 @@
         '<?=ROOT?>/assets/images/newHome/dd9908.jpg',
         '<?=ROOT?>/assets/images/newHome/ed6799.jpg',
         '<?=ROOT?>/assets/images/newHome/f828c5.jpg',
+    ];
+
+    const imageColors=[
+        '#7ab660',
+        '#244ea2',
+        '#aea5ea',
+        '#bb3f35',
+        '#dd9908',
+        '#ed6799',
+        '#f828c5',
     ];
 
     // preload images
@@ -451,14 +468,17 @@
     // Function to change background image
     function changeBackground() {
         backgroundDiv.style.backgroundImage = `url('${images[index]}')`;
+        headerColor=imageColors[index];
+        changeheaderColor();
         index = (index + 1) % images.length; // Loop back to the beginning if end is reached
+
     }
 
     // Initial call
     changeBackground();
 
     // Set interval to change background image every 3 seconds
-    setInterval(changeBackground, 5000);
+    setInterval(changeBackground, 6000);
 </script>
 
 
