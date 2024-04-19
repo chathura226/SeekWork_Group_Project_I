7beb3697194610d30250781297e35309f7c92c93
chathura226 <chathuralakshan226@gmail.com>
2024-04-19 09:47:48 +0530

    finished home page with details

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -789,50 +789,55 @@
         </div>
     </div>
 </div>
+
+<div class="best-part-cont" style="display: flex;width: 100%;margin: auto;align-items: center">
+    <img style="object-fit: cover;width: 100%" src="<?=ROOT?>/assets/images/slider/test1.jpg" >
+</div>
+<div class="best-part-cont" style="display: flex;width: 100%;margin: auto;align-items: center">
+    <img style="object-fit: cover;width: 100%" src="<?=ROOT?>/assets/images/slider/test2.jpg" >
+</div>
 <div class="best-part-cont" style="display: flex;width: 80%;margin: auto;align-items: center">
-<div class="best-part" >
-    <h1 style="font-size: 3rem">The best part? Everything.</h1>
-    <ul>
-        <li>
-            <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
-                            width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
-                                d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
-                                d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Stick
-                to your budget
-            </h2>
-            <p style="margin-left:25px;font-size: 1.2rem">Find the right service for every price point. No hourly rates, just project-based
-                pricing.</p></li>
-        <li>
-            <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
-                            width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
-                                d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
-                                d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Get
-                quality work done quickly
-            </h2>
-            <p style="margin-left:25px;font-size: 1.2rem">Hand your project over to a talented freelancer in minutes, get long-lasting results.</p>
-        </li>
-        <li>
-            <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
-                            width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
-                                d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
-                                d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Pay
-                when you're happy
-            </h2>
-            <p style="margin-left:25px;font-size: 1.2rem">Upfront quotes mean no surprises. Payments only get released when you approve.</p></li>
-        <li>
-            <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
-                            width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
-                                d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
-                                d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Count
-                on 24/7 support
-            </h2>
-            <p style="margin-left:25px;font-size: 1.2rem">Our round-the-clock support team is available to help anytime, anywhere.</p></li>
-    </ul>
+    <div class="best-part" >
+        <h1 style="font-size: 3rem">The best part? Everything.</h1>
+        <ul>
+            <li>
+                <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
+                                width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
+                                    d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
+                                    d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Stick
+                    to your budget
+                </h2>
+                <p style="margin-left:25px;font-size: 1.2rem">Find the right service for every price point. No hourly rates, just project-based
+                    pricing.</p></li>
+            <li>
+                <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
+                                width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
+                                    d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
+                                    d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Get
+                    quality work done quickly
+                </h2>
+                <p style="margin-left:25px;font-size: 1.2rem">Hand your project over to a talented freelancer in minutes, get long-lasting results.</p>
+            </li>
+            <li>
+                <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
+                                width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
+                                    d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
+                                    d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Pay
+                    when you're happy
+                </h2>
+                <p style="margin-left:25px;font-size: 1.2rem">Upfront quotes mean no surprises. Payments only get released when you approve.</p></li>
+            <li>
+                <h2 style="font-size: 2rem"><span class="" style="width:24px;height:24px;fill:#7A7D85;margin-right: 10px" aria-hidden="true"><svg
+                                width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path
+                                    d="M8 1.75C4.54822 1.75 1.75 4.54822 1.75 8C1.75 11.4518 4.54822 14.25 8 14.25C11.4518 14.25 14.25 11.4518 14.25 8C14.25 4.54822 11.4518 1.75 8 1.75ZM0.25 8C0.25 3.71979 3.71979 0.25 8 0.25C12.2802 0.25 15.75 3.71979 15.75 8C15.75 12.2802 12.2802 15.75 8 15.75C3.71979 15.75 0.25 12.2802 0.25 8Z"></path><path
+                                    d="M11.5303 5.46967C11.8232 5.76256 11.8232 6.23744 11.5303 6.53033L7.53033 10.5303C7.23744 10.8232 6.76256 10.8232 6.46967 10.5303L4.46967 8.53033C4.17678 8.23744 4.17678 7.76256 4.46967 7.46967C4.76256 7.17678 5.23744 7.17678 5.53033 7.46967L7 8.93934L10.4697 5.46967C10.7626 5.17678 11.2374 5.17678 11.5303 5.46967Z"></path></svg></span>Count
+                    on 24/7 support
+                </h2>
+                <p style="margin-left:25px;font-size: 1.2rem">Our round-the-clock support team is available to help anytime, anywhere.</p></li>
+        </ul>
+    </div>
+    <img style="object-fit: contain;width: 60%" src="<?=ROOT?>/assets/images/slider/bestpart.jpg" >
 </div>
-        <img style="object-fit: contain;width: 60%" src="<?=ROOT?>/assets/images/slider/bestpart.jpg" >
-</div>
-
-
 
 <footer style="background-color: #0a2c40">
 
